5631d52698972a44fc9e0ff450ac8a270589b962
Zach Barnes <16zacharybarnes@gmail.com>
2023-09-12 10:45:49 -0500

    fixed server error in connection.js, but now with server running, getting 400 errors on the data

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,23 @@
 require('dotenv').config();
 
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USER, 
+    process.env.DB_PW, 
+    {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
     });
+}
 
 module.exports = sequelize;
